ec3235ae3c7c2973270d86a93b8771aa5c74c52c
megarage9000 <gtom_1984@hotmail.ca>
2020-08-06 01:22:51 -0700

    87 Unity Waypoiny Utility Scripts - Quiz 7
    - Add Standard assets from 2017
    - Import music from DL Audio
    - Use waypoint utility scripts and smooth waypoint follower

---
+++ Assets/Scripts/GameSceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int gameSceneLoadDelay = 1;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadGameScene", gameSceneLoadDelay);   
+    }
+
+    private void LoadGameScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(++currentSceneIndex);
+ 
+    }
+}
