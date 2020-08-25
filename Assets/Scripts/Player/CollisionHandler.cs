8a0affe6b2c93c259956219a3f70675f4d101be6
megarage9000 <gtom_1984@hotmail.ca>
2020-08-24 22:55:50 -0700

    100. Enable a GameObject From C#
    - Add PlayerExplosion particles (a prefab variant of Explosion) to
    player
    - Modify CollisionHandler to enable PlayerExplosion and level reload on
    death Sequence

--- Assets/Scripts/Player/CollisionHandler.cs
+++ Assets/Scripts/Player/CollisionHandler.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float restartLevelDelay = 1f;
+    public GameObject DeathExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         print("Death iniatated");
@@ -13,5 +18,14 @@ public class CollisionHandler : MonoBehaviour
     private void InitateDeathSequence()
     {
         SendMessage("DisableMovement");
+        DeathExplosion.SetActive(true);
+        Invoke("RestartCurrentLevel", restartLevelDelay);
+    }
+
+    // String Referenced
+    private void RestartCurrentLevel()
+    {
+        int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentBuildIndex);
     }
 }
