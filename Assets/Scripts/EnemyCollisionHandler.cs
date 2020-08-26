486fe2d3cacf69e531b7be5fcf253a1e7b7c82bb
megarage9000 <gtom_1984@hotmail.ca>
2020-08-26 01:14:04 -0700

    103. Making Scripts Add Components
    - Remove all colliders from enemy prefabs. Edit EnemyCollisionHandler to
    add Box Colliders per enemy
    - Replace Enemy Artillery Asset

---
+++ Assets/Scripts/EnemyCollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollisionHandler : MonoBehaviour
+{
+
+    private void Start()
+    {
+        SetUpCollider();
+    }
+
+    private void SetUpCollider()
+    {
+        Collider EnemyCollider = gameObject.AddComponent<BoxCollider>();
+        EnemyCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print(gameObject.name + " Hit by " + other.name);
+        Destroy(gameObject);
+    }
+}
