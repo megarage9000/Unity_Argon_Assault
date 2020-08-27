022c678b69e7595ea9edfa0fd7213e48597ad295
megarage9000 <gtom_1984@hotmail.ca>
2020-08-27 16:21:02 -0700

    106. A ScoreBoard Class
    - Intialize a ScoreBoard.cs script class

---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scoreIncrement = 12;
+    private Text scoreText;
+    private int score;
+    
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void UpdateScore()
+    {
+        score += scoreIncrement;
+    }
+
+}
